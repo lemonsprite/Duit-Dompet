a3597ff8de3a16c67fc254d4381c6fd57517c6b5
Noor Alfath <alfath.noor17@gmail.com>
2022-03-29 02:21:50 +0700

    membuat template proyek dari mockup

---
+++ src/components/menuItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <li class="flex justify-center px-4 py-12 hover:bg-gray hover:border-r-4 border-r-amber-800">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" :width="size" :height="size">
+            <path :d="path" :fill="fill" />
+        </svg>
+    </li>
+</template>
+
+<script>
+export default {
+    props: ['fill', 'path', 'size']
+
+}
+</script>
+
+<style>
+</style>
